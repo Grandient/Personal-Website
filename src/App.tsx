41a21697892342b0b3fb55ffec86860258044965
Gavin <Gavin.Gosling1@gmail.com>
2021-01-13 23:03:28 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+
 function App() {
+  const [date, setDate] = useState(new Date().toLocaleTimeString())
+
+  useEffect(() => {
+    var timerID = setInterval(() => tick(), 1000);
+    return function cleanup() {
+        clearInterval(timerID);
+      };
+   });
+  
+  function tick() {
+    setDate(new Date().toLocaleTimeString());
+  }
+
+  const hours = new Date().getHours()
+  const isDayTime = hours > 6 && hours < 18
+
   return (
-    <div className="App">
+    <div className={isDayTime ? 'day' : 'night'}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        {date}
       </header>
     </div>
   );
