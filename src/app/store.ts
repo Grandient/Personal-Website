70cabb73bcf61478925bd9f670bf465869c2e506
Gavin <Gavin.Gosling1@gmail.com>
2021-03-25 01:17:28 -0400

    remove redux stuff for now

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,16 @@
+export {}
+
+/*
+
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+//export const store = configureStore({
+//});
 
-export type RootState = ReturnType<typeof store.getState>;
+//export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>;*/
