32486b3444686960a7e9c43ad220c3a48c5757bc
Gavin <Gavin.Gosling1@gmail.com>
2021-03-27 01:19:40 -0400

    try service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
-
+import { useEffect } from 'react';
 import '../src/App.css';
 import Head from 'next/head'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
   return <>
     <Head>
         <link rel='shortcut icon' href='/Personal-Website/favicon.ico'/>
