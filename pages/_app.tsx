c1d8660223a44ae12d080a8791be495de0d23fd5
Gavin <Gavin.Gosling1@gmail.com>
2021-03-27 00:08:23 -0400

    more fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ export default function MyApp({ Component, pageProps }) {
     <Head>
         <link rel='shortcut icon' href='/Personal-Website/favicon.ico'/>
         <link rel="manifest" href="/Personal-Website/manifest.json" />
+        <link rel="apple-touch-icon" href="/Personal-Website/logo192.png" />
         <meta name="viewport" content="width=device-width,initial-scale=1"/>
         <title>Gavin Gosling</title>
     </Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,14 +11,11 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="theme-color" content="#000000" />
         <meta
         name="description"
         content="Gavin Gosling's space and portfolio" 
         />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" 
             rel="stylesheet"/>
